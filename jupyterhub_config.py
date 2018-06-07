0170e7781fa76d5cbd3bfe445d0c83559cb44a27
AlexMontoya <montoyaa@dschool.fr>
2018-06-07 15:24:35 +0200

    Adding proxy config

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,2 +1,6 @@
 c.KubeSpawner.start_timeout = 180
 c.KubeSpawner.http_timeout = 120
+
+import os
+os.environ['http_proxy'] = "http://user:passwd@host:port" 
+os.environ['https_proxy'] = "https://user:passwd@host:port" 
